7127f26dee4f6162c2668d299ee7477e5fa00372
Matteo Tiberti <matteo.tiberti@gmail.com>
2026-02-19 15:12:46 +0100

    added folder

---
+++ downstream_analysis_mavisp_templates/dotplot/do.sh
@@ -0,0 +1,8 @@
+module load python/3.10/modulefile
+csv=$1
+# change the csv path and the number of mutations you want to plot on the x-axis
+# suggested not to use more than 50, otherwise you need to chage the figuresize;
+# for more information about available options: python dot_plot.py -h
+python dot_plot_2025.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof 
+python lolliplot.py -i mechanistic_indicators_out.csv -x 50 -s
+python filter_pLDDT.py -i $csv -m mechanistic_indicators_out.csv -t 70
