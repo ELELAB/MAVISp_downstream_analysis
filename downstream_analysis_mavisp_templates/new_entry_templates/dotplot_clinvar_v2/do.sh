2a1d21ee13f39783e958954a756318254b6839ef
Eleni Kiahaki <s222777@dtu.dk>
2026-04-29 10:54:04 +0200

    updated do.sh in new_entry templates

--- downstream_analysis_mavisp_templates/new_entry_templates/dotplot_clinvar_v2/do.sh
+++ downstream_analysis_mavisp_templates/new_entry_templates/dotplot_clinvar_v2/do.sh
@@ -3,6 +3,27 @@ csv=$1
 # change the csv path and the number of mutations you want to plot on the x-axis
 # suggested not to use more than 50, otherwise you need to chage the figuresize;
 # for more information about available options: python dot_plot.py -h
-python dot_plot_v2.py -i $csv -x  50  -pltD -pltR -pltC benign likely_benign pathogenic likely_pathogenic -vep none -cct germline -v dictionary.csv  
-python lolliplot.py -i mechanistic_indicators_out.csv -x 50 -s
-python filter_pLDDT.py -i $csv -m mechanistic_indicators_out.csv -t 70
+
+# Germline classification results:
+python dot_plot_v2.py -i $csv -x  50  -pltD -pltR -pltC benign likely_benign pathogenic likely_pathogenic -vep none -cct germline -v dictionary.csv -o dot_plot_germline
+mv mechanistic_indicators_out.csv mechanistic_indicators_out_germline.csv
+mv log.txt log_germline.txt
+python lolliplot.py -i mechanistic_indicators_out_germline.csv -x 50 -s
+mv lolliplot.pdf lolliplot_germline.pdf
+python filter_pLDDT.py -i $csv -m mechanistic_indicators_out_germline.csv -t 70 -o mech_plddt_germline
+
+# Oncogenicity classification results:
+python dot_plot_v2.py -i $csv -x  50  -pltD -pltR -pltC benign likely_benign oncogenic likely_oncogenic -vep none -cct oncogenicity -v oncodict.csv -o dot_plot_oncogenicity 
+mv mechanistic_indicators_out.csv mechanistic_indicators_out_oncogenicity.csv
+mv log.txt log_oncogenicity.txt
+python lolliplot.py -i mechanistic_indicators_out_oncogenicity.csv -x 50
+mv lolliplot.pdf lolliplot_oncogenicity.pdf
+python filter_pLDDT.py -i $csv -m mechanistic_indicators_out_oncogenicity.csv -t 70 -o mech_plddt_oncogenicity
+
+# Clinical impact classification results:
+python dot_plot_v2.py -i $csv -x  50  -pltD -pltR -pltC benign potential strong -vep none -cct clinical_impact -v clinicaldict.csv -o dot_plot_clinical_impact
+mv mechanistic_indicators_out.csv mechanistic_indicators_out_clinical_impact.csv
+mv log.txt log_clinical_impact.txt
+python lolliplot.py -i mechanistic_indicators_out_clinical_impact.csv -x 50
+mv lolliplot.pdf lolliplot_clinical_impact.pdf
+python filter_pLDDT.py -i $csv -m mechanistic_indicators_out_clinical_impact.csv -t 70 -o mech_plddt_clinical_impact
--- downstream_analysis_mavisp_templates/new_entry_templates/dotplot_v2/do.sh
+++ downstream_analysis_mavisp_templates/new_entry_templates/dotplot_v2/do.sh
@@ -3,6 +3,18 @@ csv=$1
 # change the csv path and the number of mutations you want to plot on the x-axis
 # suggested not to use more than 50, otherwise you need to chage the figuresize;
 # for more information about available options: python dot_plot.py -h
-python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct germline -v dictionary.csv 
+
+# Germline classification dotplot
+python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct germline -v dictionary.csv -o dot_plot_germline
+mv log.txt log_germline.txt
+
 python lolliplot.py -i mechanistic_indicators_out.csv -x 50 -s
 python filter_pLDDT.py -i $csv -m mechanistic_indicators_out.csv -t 70
+
+# Oncogenicity dotplot
+python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct oncogenicity -v oncodict.csv -o dot_plot_oncogenicity
+mv log.txt log_oncogenicity.txt
+
+# Clinical impact dotplot
+python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct clinical_impact -v clinicaldict.csv -o dot_plot_clinical_impact
+mv log.txt log_clinical_impact.txt
