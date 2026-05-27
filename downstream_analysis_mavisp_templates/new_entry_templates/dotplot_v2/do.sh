64a0d76e539bc4897a42cfa0034eeac0fbb877f9
Eleni Kiahaki <s222777@dtu.dk>
2026-05-27 10:58:14 +0200

    adjustments in lolliplot.py and dotplot_v2.py

--- downstream_analysis_mavisp_templates/new_entry_templates/dotplot_v2/do.sh
+++ downstream_analysis_mavisp_templates/new_entry_templates/dotplot_v2/do.sh
@@ -6,15 +6,13 @@ csv=$1
 
 # Germline classification dotplot
 python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct germline -v dictionary.csv -o dot_plot_germline
-mv log.txt log_germline.txt
 
-python lolliplot.py -i mechanistic_indicators_out.csv -x 50 -s
-python filter_pLDDT.py -i $csv -m mechanistic_indicators_out.csv -t 70
+python lolliplot.py -i mechanistic_indicators_out_germline.csv -x 50 -s
+python filter_pLDDT.py -i $csv -m mechanistic_indicators_out_germline.csv -t 70
 
 # Oncogenicity dotplot
 python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct oncogenicity -v oncodict.csv -o dot_plot_oncogenicity
-mv log.txt log_oncogenicity.txt
 
 # Clinical impact dotplot
 python dot_plot_v2.py -i $csv -x  50 -pltD -pltR -colC -vep -lgof -cct clinical_impact -v clinicaldict.csv -o dot_plot_clinical_impact
-mv log.txt log_clinical_impact.txt
+
