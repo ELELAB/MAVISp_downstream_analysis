c8050200c3cba52b52672879b9f1d7a1453b2cde
Matteo Tiberti <matteo.tiberti@gmail.com>
2026-07-07 10:05:48 +0200

    changed example

--- dot_plot/examples_v2/ensemble_mode_denovop/do.sh
+++ dot_plot/examples_v2/ensemble_mode_denovop/do.sh
@@ -1,3 +1,3 @@
 module load python
 
-python ../../dot_plot_v2.py -i CREBBP-ensemble_mode.csv -x  50 -pltD -pltR -pltP -colC -vep -lgof -cct germline -v dictionary.csv 
+python ../../dot_plot_v2.py -i FH-ensemble_mode.csv -x  50 -pltD -pltR -pltP -colC -vep -lgof -cct germline -v dictionary.csv 
